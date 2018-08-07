2f0e18c4a695c00fb8b5718a5909de519620d2a7
ASISTA\rfernandez <edisonfernandez.1988@hotmail.com>
2018-08-07 18:20:49 -0500

    cambiosFire

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,18 +10,28 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) { 
-    
     //Leer json
-    this.http.get('assets/data/data-pagina.json').
-    subscribe( (resp: InfoPagina ) => {
-      
-      this.cargada = true;
-      this.info = resp;
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
-    });
+  private cargarInfo() {
+    this.http.get('assets/data/data-pagina.json').
+      subscribe((resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+      });
   }
   
-  
+  private cargarEquipo() {
+    this.http.get('https://angular-html-9adb3.firebaseio.com/team.json').
+    subscribe( ( resp: any[] ) => {
+       this.equipo = resp; 
+       console.log(resp);
+       
+    });
+  }
 }
