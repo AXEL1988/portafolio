f61fb7f3128604df45e92803f39770ebd7352780
Edison Fernandez <edisonfernandez.1988@hotmail.com>
2018-08-07 01:03:01 -0500

    Seccion 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -13,10 +16,13 @@ import { PortafolioComponent } from './pages/portafolio/portafolio.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
